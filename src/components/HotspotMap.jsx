cb689bb1ceee8aaa28eb0e2d876b6c535eda8015
Shivam Gupta <guptajishiv12@gmail.com>
2025-04-17 13:32:21 +0530

    refactor: update HotspotMap to include danger zone as a red circle

--- src/components/HotspotMap.jsx
+++ src/components/HotspotMap.jsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Circle, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import { Marker, Popup } from "react-leaflet";
-import L from "leaflet"; // Optional: for custom icons
-import { Polygon } from "react-leaflet";
+import L from 'leaflet'; // Optional: for custom icons
 
+// Define coordinates for the danger zone (Red Circle for DTU)
 const delhiZone = [
-  [28.7510, 77.1135],
-  // [28.7515, 77.1200],
-  // [28.7475, 77.1205],
-  // [28.7465, 77.1145],
+  { lat: 28.7510, lon: 77.1135, radius: 50 }, // Coordinates for DTU (radius in meters)
 ];
 
-
-
-
-
-
-
-// Define red icon for markers
+// Define red icon for markers (not used for circles, but you can add markers later)
 const redIcon = new L.Icon({
   iconUrl:
     "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png",
@@ -30,29 +20,36 @@ const redIcon = new L.Icon({
   shadowSize: [41, 41],
 });
 
-
-
 const HotspotMap = () => {
   return (
     <div style={{ height: '400px', width: '100%' }}>
       <MapContainer
-        
-        center={[20.5937, 78.9629]}
-        zoom={5}
+        center={[28.6139, 77.2090]} // Centered on Delhi
+        zoom={13}
         style={{ height: '100%', width: '100%' }}
       >
         <TileLayer
-  url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}"
-  attribution='Tiles &copy; Esri, Data &copy; OpenStreetMap contributors'
-/>
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}"
+          attribution="Tiles &copy; Esri, Data &copy; OpenStreetMap contributors"
+        />
 
-<Polygon
-  positions={delhiZone}
-  pathOptions={{ color: "red", fillColor: "red", fillOpacity: 0.4 }}
-/>
-
-
-
+        {/* Danger Zone - Red Circle */}
+        {delhiZone.map((zone, index) => (
+          <Circle
+            key={index}
+            center={[zone.lat, zone.lon]}
+            radius={zone.radius} // radius in meters
+            pathOptions={{
+              color: 'red',
+              fillColor: 'red',
+              fillOpacity: 0.4,
+            }}
+          >
+            <Popup>
+              Danger Zone - High Alert
+            </Popup>
+          </Circle>
+        ))}
       </MapContainer>
     </div>
   );
